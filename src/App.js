e442d81c46e13d01efd8ab319ee7dd797fadfcbd
Nexfar <nexfar@Nexfars-MacBook-Pro.local>
2020-11-03 22:20:17 -0300

    crate header to featured movie

--- src/App.js
+++ src/App.js
@@ -3,26 +3,49 @@ import {useEffect, useState} from "react";
 
 import TmdbApi from './api/TmdbApi'
 import MovieRow from "./components/MovieRow";
+import FeaturedMove from "./components/FeaturedMove";
 
 import './App.css'
 
 export default function App()  {
+    const [loading, setLoading] = useState(true);
     const [moveList, setMoveList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect(() => {
         (async () => {
-            await loadAll();;
+            const resultMoveList = await loadAll();
+            setMoveList(resultMoveList);
+
+            if (resultMoveList) {
+                const resultFeaturedMove = await randomFeatured(resultMoveList);
+                setFeaturedData(resultFeaturedMove);
+                setLoading(false);
+            }
         })();
     }, [])
 
     const loadAll = async () => {
-        const list = await TmdbApi.getHomeList();
-        setMoveList(list);
+        return await TmdbApi.getHomeList();
     }
+
+    const randomFeatured = async (list) => {
+        const originals = list.filter(move => move.slug === 'originals');
+        const randomItem = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+        setFeaturedData(originals[0].items.results[randomItem])
+    }
+
+
+    if (loading) return null;
 
     return (
         <div className='pages'>
           <section className='lists'>
+
+              {featuredData &&
+                <FeaturedMove item={featuredData} />
+              }
+
               {moveList.map((item) => (
                   <MovieRow
                       key={item.title}
---
+++ src/components/FeaturedMove.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './FeaturedMove.css'
+
+export default function FeaturedMove({ featuredData }) {
+    return (
+        <div>Filme em destaque</div>
+    )
+}
